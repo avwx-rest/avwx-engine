5e0b185f74e5cac402e000e0a0273edc88f38091
Michael duPont <michael@mdupont.com>
2018-04-21 00:05:31 -0400

    Setup reads requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -7,18 +7,22 @@ avwx package metadata
 
 from setuptools import setup
 
+def list_requirements() -> [str]:
+    """
+    Read requirements from requirements.txt
+    """
+    with open('requirements.txt') as fin:
+        return fin.readlines()
+
 setup(
     name='avwx-engine',
-    version='0.11.3',
+    version='0.11.4',
     description='Aviation weather report parsing library',
     url='https://github.com/flyinactor91/AVWX-Engine',
     author='Michael duPont',
     author_email='michael@mdupont.com',
     license='MIT',
-    install_requires=[
-        'requests~=2.18.2',
-        'xmltodict~=0.11.0'
-    ],
+    install_requires=list_requirements(),
     packages=[
         'avwx'
     ],
