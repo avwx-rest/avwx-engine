1d97debf772273f406268ecb9408a53fe62ec295
flyinactor91 <michael@mdupont.com>
2019-11-22 23:58:34 -0500

    Add Python3.8 to tests

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
 import nox
 
 
-@nox.session(python=["3.6", "3.7"])
+@nox.session(python=["3.6", "3.7", "3.8"])
 def tests(session):
     session.install("-e", ".[scipy]")
-    session.install("pytest~=5.0")
+    session.install("pytest~=5.3")
     session.run("pytest", "--disable-warnings")
