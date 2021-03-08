0152a5ffe244ca3104b4908d41f0743694afb102
Michael duPont <flyinactor91@gmail.com>
2021-03-07 22:54:13 -0500

    Update station data

--- avwx/station/meta.py
+++ avwx/station/meta.py
@@ -4,7 +4,7 @@ Shared list and metadata
 
 from avwx.load_utils import LazyLoad
 
-__LAST_UPDATED__ = "2021-01-24"
+__LAST_UPDATED__ = "2021-03-07"
 
 # Lazy data loading to speed up import times for unused features
 STATIONS = LazyLoad("stations")
